0c6114184386c96efcad94efe42009cf58b6b45d
Eugene Bogun <eugenebogun@gmail.com>
2015-10-25 01:20:16 +0300

    Change configs controller

--- client/app/controllers/users/usersCtrl.js
+++ client/app/controllers/users/usersCtrl.js
@@ -1,5 +1,9 @@
 angular.module("app.controllers.usersCtrl", [])
-    .controller('usersCtrl', function ($scope, $log) {
+    .controller('usersCtrl', function ($scope, $log, $http) {
         $log.debug('init usersCtrl');
+
+        $http.get('/mock/users.json').then(function (response) {
+            $scope.items = response.data.items;
+        });
 
     });

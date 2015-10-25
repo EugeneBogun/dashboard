958b30981bbd2ceba0163ea0523ba8cf23f20384
Eugene Bogun <eugenebogun@gmail.com>
2015-10-25 15:24:09 +0200

    add active class to menu

--- client/app/controllers/navigate/navigateCtrl.js
+++ client/app/controllers/navigate/navigateCtrl.js
@@ -1,5 +1,8 @@
 angular.module("app.controllers.navigateCtrl", [])
-    .controller('navigateCtrl', function ($scope, $log) {
+    .controller('navigateCtrl', function ($scope, $log, $location) {
         $log.debug('init navigateCtrl');
 
+        $scope.getClass = function (path) {
+            return $location.path() == path?'active':'';
+        };
     });
